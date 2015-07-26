c518c5fa8b55a937db6f5ba7baac23c2be52d0aa
DJ <dj.anderson06@gmail.com>
2015-07-25 22:35:00 -0400

    folder change
    Changed from landon_games to kid_games

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    
+    var originalState = $("#game").clone();
+    
+//  effects for all wrong colors for levels 1,3, and 5.
+    $('#level1 .wrong, #level3 .wrong, #level5 .wrong').click(function() {
+        $(this).effect('explode');
+    });
+    
+//  effects for all wrong colors for levels 2, 4, and 6.    
+    $('#level2 .wrong, #level4 .wrong, #level6 .wrong').click(function() {
+        $(this).slideUp('slow');
+    });
+    
+//  Play button
+    $('.play').click(function() {
+        $('#start').slideUp('slow');
+        $('#level1').slideDown('slow')
+    });
+    
+//  effect for right color level 1
+    $('#level1 .right').click(function() {
+        $(this).effect('bounce', 'slow');
+        $('#level1').delay(550).slideUp('slow');
+        $('#level2').delay(1200).slideDown('slow');
+    });
+    
+// effect for right color level 2
+    $('#level2 .right').click(function() {
+        $(this).effect('shake', 'slow');
+        $('#level2').delay(550).slideUp('slow');
+        $('#level3').delay(1200).slideDown('slow');
+    });
+    
+//  effect for right color level 3
+    $('#level3 .right').click(function() {
+        $(this).effect('slide', 'slow');
+        $('#level3').delay(550).slideUp('slow');
+        $('#level4').delay(1200).slideDown('slow');
+    });
+    
+//  effect for right color level 4
+    $('#level4 .right').click(function() {
+        $(this).effect('pulsate', 'slow');
+        $('#level4').delay(550).slideUp('slow');
+        $('#level5').delay(1200).slideDown('slow');
+    });
+    
+//  effect for right color level 5
+    $('#level5 .right').click(function() {
+        $(this).effect('bounce', 'slow');
+        $('#level5').delay(550).slideUp('slow');
+        $('#level6').delay(1200).slideDown('slow');
+    });
+    
+//  effect for right color level 6
+    $('#level6 .right').click(function() {
+        $(this).effect('shake', 'slow');
+        $('#level6').delay(550).slideUp('slow');
+        $('#end').delay(1200).slideDown('slow');
+    });
+    
+//  button to restart the game
+    $('.playAgain').click(function() {
+//  function to restart the game
+        $('#game').replaceWith(originalState);
+        $('#level1').slideDown('slow');
+    });
+});
